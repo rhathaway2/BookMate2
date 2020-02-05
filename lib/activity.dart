b57870a69970262a6e2a11861eacb02ab9d56704
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-05 09:44:24 -0800

    Initial skeleton of Activity widget

---
+++ lib/activity.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/*
+
+*/
+class ActivityPage extends StatefulWidget{
+  ActivityPage();
+
+  @override
+  _ActivityPageState createState() => _ActivityPageState();
+
+}
+
+/*
+
+*/
+class _ActivityPageState extends State<ActivityPage>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
