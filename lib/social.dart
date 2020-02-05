e769e5cd233a2c520310bc487fef8011288733ab
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-05 09:51:23 -0800

    Initial social widget

---
+++ lib/social.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+/*
+
+*/
+class SocialPage extends StatefulWidget{
+  SocialPage();
+
+  @override
+  _SocialPageState createState() => _SocialPageState();
+
+}
+
+/*
+
+*/
+class _SocialPageState extends State<SocialPage>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
