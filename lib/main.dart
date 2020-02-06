6ed8e05d6117f5e1480fe847eef715faa9fee1cb
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-05 20:09:23 -0800

    Update navigation to tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
-import 'package:circular_bottom_navigation/tab_item.dart';
 
 //imports for tabs
 import 'library.dart';
-import 'social.dart';
-import 'activity.dart';
-
+//import 'social.dart';
+//import 'activity.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  //List of widgets to be displayed for each tab
+  final List<Widget> _pages = [
+    PlaceHolderWidget(),
+    PlaceHolderWidget(),
+    LibraryPage(),
+  ];
+
+  final List<Tab> _tabs = [
+    Tab(text: "Activity", icon: Icon(Icons.poll, color: Colors.cyan)),
+    Tab(text: "Social", icon: Icon(Icons.people, color: Colors.cyan)),
+    Tab(text: "Library", icon: Icon(Icons.book, color: Colors.cyan)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BookMate',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text("BookMate"),
+            textTheme: TextTheme(
+                title: TextStyle(
+                    color: Colors.cyan, fontSize: 25.0, fontFamily: 'roboto')),
+            bottom: TabBar(labelColor: Colors.cyan, tabs: _tabs),
+          ),
+          body: TabBarView(children: _pages),
+        ),
       ),
-      home: MyHomePage(title: 'BookMate'),
     );
   }
 }
 
-/*
-
-*/
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-
-/*
-
-*/
-class _MyHomePageState extends State<MyHomePage> {
-
-  static int _currentIndex = 0; //index of current tab
-  //List of widgets to be displayed for each tab
-  final List<Widget> _pages = [
-    PlaceHolderWidget(Colors.green),
-    PlaceHolderWidget(Colors.blue),
-    PlaceHolderWidget(Colors.red),
-  ];
-
-  static void changePage(int index){
-    _navigationController.value = index;
-    _currentIndex = index;
-  }
-
-  //controller for circular bottom nav bar
-  static CircularBottomNavigationController _navigationController = 
-  new CircularBottomNavigationController(_currentIndex);
-  //List of tabItems for circulatr bottom navigation bar
-  List<TabItem> tabItems =  List.of([
-    new TabItem(Icons.poll, "Activity", Colors.cyan, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.cyan),),
-    new TabItem(Icons.people, "Social", Colors.cyan, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.cyan)),
-    new TabItem(Icons.book, "Library", Colors.cyan, labelStyle: TextStyle(fontWeight: FontWeight.normal, color: Colors.cyan)),
-  ]);
-
+class PlaceHolderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      //body
-      body: _pages[_currentIndex],
-
-      //Adds Navigation bar to the bottom of the app screen
-      bottomNavigationBar: CircularBottomNavigation(
-        tabItems,
-        controller: _navigationController,
-        selectedCallback: (int selectedPos){
-          //call set state and update index of current page
-          setState(() {
-            _navigationController.value = selectedPos;
-            _currentIndex = selectedPos;
-          });
-        },
-      )
-    );
-  }
-}
-
-
-
-class PlaceHolderWidget extends StatelessWidget{
-  final Color color;
-  PlaceHolderWidget(this.color);
-
-  @override
-  Widget build(BuildContext context){
     return Container(
-      color: color,
+      color: Colors.white,
     );
   }
 }
