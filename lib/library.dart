b1e9b83e58e2755d162a4db57e666bd945054e33
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-05 20:09:02 -0800

    Add bottom nav bar buttons and test books

--- lib/library.dart
+++ lib/library.dart
@@ -4,48 +4,101 @@ import 'package:flutter/material.dart';
 //file imports
 import 'classes.dart';
 
-
 /*
 The purpose of this class is to display all the books in the users library
 It contains 2 lists of books booklist and favorited
 */
-class LibraryPage extends StatefulWidget{
-  LibraryPage(this.booklist, this.favorited);
+class LibraryPage extends StatefulWidget {
 
-  final List<Book> booklist;
-  final List<Book> favorited;
+  //List of books to display
+  static Book test1 = new Book(
+    title: "Ender's Game",
+    author: "Orson Scott Card",
+    pages: 350,
+    isbn: 1234567890,
+    rating: 4.67,
+    coverImageURL: "https://raw.githubusercontent.com/rhathaway2/BookMate/master/imgs/endersgame.jpg"
+  );  
+  static Book test2 = new Book(
+    title: "The Eye of the World",
+    author: "Robert Jordan",
+    pages: 465,
+    isbn: 0987654321,
+    rating: 4.89,
+    coverImageURL: "https://raw.githubusercontent.com/rhathaway2/BookMate/master/imgs/eyeoftheworld.jpg"
+  );  
+  static Book test3 = new Book(
+    title: "Dune",
+    author: "Frank Herbert",
+    pages: 542,
+    isbn: 0987654321,
+    rating: 4.37,
+    coverImageURL: "https://raw.githubusercontent.com/rhathaway2/BookMate/master/imgs/dune.jpg"
+  );  
+  static Book test4 = new Book(
+    title: "Hyperion",
+    author: "Dan Simmons",
+    pages: 398,
+    isbn: 0987654321,
+    rating: 4.74,
+    coverImageURL: "https://raw.githubusercontent.com/rhathaway2/BookMate/master/imgs/hyperion.jpg"
+  );  
+
+
+
+  final List<Book> booklist=[test1, test2, test3, test4];
+  final List<Book> favorited=[];
 
   @override
-  _LibraryPageState createState() => _LibraryPageState(
-    this.booklist, 
-    this.favorited
-  );
-
+  _LibraryPageState createState() =>
+      _LibraryPageState(booklist: this.booklist, favorited: this.favorited);
 }
 
 /*
 
 */
-class _LibraryPageState extends State<LibraryPage>{
+class _LibraryPageState extends State<LibraryPage> {
   //Variables
   List<Book> booklist;
   List<Book> favorited;
 
   //Constructor
-  _LibraryPageState(this.booklist, this.favorited);
+  _LibraryPageState({this.booklist, this.favorited});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _buildBookList()
-    );
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 4.0,
+          icon: const Icon(Icons.add),
+          label: const Text('Add Book'),
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
+          ),
+        ),
+        body: _buildBookList());
   }
 
   //function to build book list
-  Widget _buildBookList(){
+  Widget _buildBookList() {
     return ListView.builder(
-      key: UniqueKey(),
-      padding: const EdgeInsets.all(10.0),
+        key: UniqueKey(),
+        padding: const EdgeInsets.all(10.0),
         //number of items is the size of our list
         itemCount: booklist.length,
         itemBuilder: (context, i) {
@@ -57,9 +110,8 @@ class _LibraryPageState extends State<LibraryPage>{
   }
 }
 
-
 //Class for each book card
-class BookCard extends StatefulWidget{
+class BookCard extends StatefulWidget {
   final Book book; //book displayed on card
   final bool isFavorited;
   BookCard(this.book, this.isFavorited); //constructor
@@ -69,13 +121,13 @@ class BookCard extends StatefulWidget{
 }
 
 //class for the state of the book card
-class _BookCardState extends State<BookCard>{
+class _BookCardState extends State<BookCard> {
   Book book; //book being displayed
   bool isFavorited;
-  _BookCardState(this.book, this.isFavorited); 
+  _BookCardState(this.book, this.isFavorited);
 
   //init state function
-  void initState(){
+  void initState() {
     super.initState();
   }
 
@@ -85,7 +137,7 @@ class _BookCardState extends State<BookCard>{
       width: 100.0,
       height: 150.0,
       decoration: BoxDecoration(
-        shape: BoxShape.rectangle ,
+        shape: BoxShape.rectangle,
         image: DecorationImage(
           fit: BoxFit.cover,
           image: NetworkImage(this.book.coverImageURL ?? ''),
@@ -95,70 +147,71 @@ class _BookCardState extends State<BookCard>{
   }
 
   //get book card
-  Widget get bookCard{
+  Widget get bookCard {
     return Container(
-      width: 400.0,
-      height: 160.0,
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 1.0,
-            bottom: 1.0,
-            left: 35.0,
-          ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text(widget.book.title,
-                style: Theme.of(context).textTheme.headline),
-            Text(widget.book.author,
-                style: Theme.of(context).textTheme.subhead),
-            Text("${widget.book.pages.toString()} pages",
-                style: Theme.of(context).textTheme.subhead),
-            Row(
+        width: 400.0,
+        height: 160.0,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 1.0,
+              bottom: 1.0,
+              left: 35.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                Icon(
-                  Icons.star,
-                ),
-                Text(': ${widget.book.rating} / 5'),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 100.0,
-                  ),
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      setState(() {
-                        isFavorited = !isFavorited;
-                      });
-                    },
-                    child: Icon(isFavorited ? Icons.favorite : Icons.favorite_border),
-                    backgroundColor: Colors.pink,
-                    mini: true,
-                  ),
+                Text(widget.book.title,
+                    style: Theme.of(context).textTheme.headline),
+                Text(widget.book.author,
+                    style: Theme.of(context).textTheme.subhead),
+                Text("${widget.book.pages.toString()} pages",
+                    style: Theme.of(context).textTheme.subhead),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.star,
+                    ),
+                    Text(': ${widget.book.rating} / 5'),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 100.0,
+                      ),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            isFavorited = !isFavorited;
+                          });
+                        },
+                        child: Icon(isFavorited
+                            ? Icons.favorite
+                            : Icons.favorite_border),
+                        backgroundColor: Colors.pink,
+                        mini: true,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
-      )
-    );
+          ),
+        ));
   }
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 3.0),
       child: Container(
-      height: 160.0,
-      width: 200.0,
-      child: Stack(children: <Widget>[
-        Positioned(left: 70.0, child: bookCard),
-        Positioned(top: 5.0, child: coverImage),
-      ],)
-    ),
+          height: 160.0,
+          width: 200.0,
+          child: Stack(
+            children: <Widget>[
+              Positioned(left: 70.0, child: bookCard),
+              Positioned(top: 5.0, child: coverImage),
+            ],
+          )),
     );
   }
 }
