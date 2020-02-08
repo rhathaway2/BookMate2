f7a54d6e560edcb5a88f43d9a7b5d936998e0a14
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-07 19:50:30 -0800

    Now passes user info to home page

--- lib/splash.dart
+++ lib/splash.dart
@@ -29,6 +29,7 @@ class _SplashPageState extends State<SplashPage> {
                               context,
                               MaterialPageRoute(
                                   builder: (context) => HomePage(
+                                        user: result,
                                         uid: currentUser.uid,
                                       ))))
                       .catchError((err) => print(err))
