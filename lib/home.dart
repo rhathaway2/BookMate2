b267c05bd9a4e42cd45014b3cd90cff574b01f45
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-07 19:48:57 -0800

    Add side drawer menu

--- lib/home.dart
+++ lib/home.dart
@@ -1,23 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'library.dart';
 import 'activity.dart';
 import 'social.dart';
+import 'login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
-  
-  HomePage({Key key, this.uid}) : super(key:key);
+  HomePage({Key key, this.uid, this.user}) : super(key: key);
   final String uid;
+  final DocumentSnapshot user;
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>{
-
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
   final List<Tab> _tabs = [
     Tab(text: "Activity", icon: Icon(Icons.poll, color: Colors.cyan)),
     Tab(text: "Social", icon: Icon(Icons.people, color: Colors.cyan)),
     Tab(text: "Library", icon: Icon(Icons.book, color: Colors.cyan)),
   ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +37,7 @@ class _HomePageState extends State<HomePage>{
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
+          drawer: new SideDrawer(user: widget.user),
           appBar: AppBar(
             backgroundColor: Colors.white,
             title: Text("BookMate"),
@@ -41,8 +46,68 @@ class _HomePageState extends State<HomePage>{
                     color: Colors.cyan, fontSize: 25.0, fontFamily: 'roboto')),
             bottom: TabBar(labelColor: Colors.cyan, tabs: _tabs),
           ),
-          body: TabBarView(children: _pages),
+          body: TabBarView(
+            children: _pages,
+          ),
         ),
+      ),
+    );
+  }
+}
+
+/*
+Side drawer menu
+*/
+class SideDrawer extends StatelessWidget {
+  SideDrawer({this.user});
+  final DocumentSnapshot user;
+
+  /*
+    function to log out current firebase user
+    */
+  Future<void> _userLogOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text("${user["fname"]} ${user["surname"]}"),
+            accountEmail: Text("${user["email"]}"),
+            currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("${user["fname"][0]}${user["surname"][0]}",
+                    style: TextStyle(fontSize: 40.0))),
+          ),
+          ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Profile"),
+              onTap: () {
+                //do stuff
+                //exit drawer
+                Navigator.of(context).pop();
+              }),
+          ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Text("Log out"),
+              onTap: () {
+                //do stuff
+                _userLogOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => LoginPage()
+                    ),
+                    (_) => false);
+              }),
+        ],
       ),
     );
   }
