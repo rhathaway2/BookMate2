d2b936f9fd42ad71ee5f9fbca8f24f5f22901cae
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-17 18:32:19 -0800

    Moves constants for dark and light theme to seperate file

---
+++ lib/constants.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+class Constants{
+  //Colors for theme
+  static Color lightPrimary = Color(0xfffcfcff);
+  static Color darkPrimary = Colors.black;
+  static Color lightAccent = Colors.teal[200];
+  static Color darkAccent = Colors.teal[200];
+  static Color lightBG = Color(0xfffcfcff);
+  static Color darkBG = Colors.black;
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    accentColor:  lightAccent,
+    cursorColor: lightAccent,
+    scaffoldBackgroundColor: lightBG,
+    bottomAppBarTheme: BottomAppBarTheme(
+      elevation: 0,
+      color: lightBG
+    ),
+    cardTheme: CardTheme(color: lightPrimary),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        title: TextStyle(
+          color: darkBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    accentColor: darkAccent,
+    scaffoldBackgroundColor: darkBG,
+    cursorColor: darkAccent,
+    bottomAppBarTheme: BottomAppBarTheme(
+      elevation: 0,
+      color: darkBG
+    ),
+    cardTheme: CardTheme(color: darkPrimary),
+    appBarTheme: AppBarTheme(
+      elevation: 0,
+      textTheme: TextTheme(
+        title: TextStyle(
+          color: lightBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    ),
+  );
+
+}
