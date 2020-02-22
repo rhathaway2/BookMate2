c3e8e9d7383d1cd048163443b018be12beb5c4a8
rhathaway2 <ryanhathaway777@gmail.com>
2020-02-22 15:26:59 -0800

    Remove unused import

--- lib/search.dart
+++ lib/search.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:progress_dialog/progress_dialog.dart';
 import 'classes.dart';
 import 'package:http/http.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
